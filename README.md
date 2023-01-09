aee164c46f4fb3342a6fa5ac8826afd220bf2f20
Dengel <52581244+DengelLionel@users.noreply.github.com>
2023-01-08 20:52:13 -0500

    comentando algo mas en README

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
     - **openMenu:** Es para abrir el menú del responsive y mejorar la accesesibilidad de el responsive **mobile**
     - **children:** Puedes agregar cualquier elemento como por ejemplo en next js Link
 
+    `
+    <NavMobileTypeOne openMenu={openMenu}>
+    
+    </NavMobileTypeOne>
+    
+    `
+
 ## COMPONENTES HEADER
 1. **HeaderMobileTypeOne**
 
@@ -14,3 +21,10 @@
     - **children:** Puedes agregar cualquier elemento
     - **colorHeader:** No es obligatorio añadir color de fondo de tu elemento header.
     - **logo:** Añada elemento img o en caso de next js Image. 
+
+    
+    `
+    <HeaderMobileTypeOne logo={<img alt="img" src="#" className='text-white'/>} colorHeader='bg-sky-800' openMenu={openMenu} setOpenMenu={()=>setOpenMenu(!openMenu)}>
+    </HeaderMobileTypeOne>
+    `
+    ***IMPORTANTE: openMenu y setOpenMenu deben ser creados por usted mismo ya sea con useState o context o como a usted le guste.***
--- src/App.tsx
+++ src/App.tsx
@@ -13,23 +13,23 @@ const [openSub,setOpenSub]=useState<any>({id:null,state:false})
     <HeaderMobileTypeOne logo={<h2 className='text-white'>Hola mundo</h2>} colorHeader='bg-sky-800' openMenu={openMenu} setOpenMenu={()=>setOpenMenu(!openMenu)}>
     
     <NavMobileTypeOne openMenu={openMenu}>
-    <NavLink 
-    index={[{id:1,state:false}]}
-    subCategory={true}
-    openSubCategory={openSub}
-    setOpenSubCategory={()=>setOpenSub({id:1,state:!openSub.state})}
-    title="Hola mundo Hola mundo"
-    titleSubNav={[{id:1,title:"hola mm"},{id:2,title:"mundo"}]}/>
-     <NavLink 
-     index={[{id:2,state:false}]}
-    subCategory={true}
-    openSubCategory={openSub}
-    setOpenSubCategory={()=>setOpenSub(!openSub)}
-    title="Hola mundo Hola mundo"
-    titleSubNav={[{id:1,title:"hola deb"},{id:2,title:"jajhaj"}]}/>
+    
     </NavMobileTypeOne>
     </HeaderMobileTypeOne>
-   
+ /*   <NavLink 
+   index={[{id:1,state:false}]}
+   subCategory={true}
+   openSubCategory={openSub}
+   setOpenSubCategory={()=>setOpenSub({id:1,state:!openSub.state})}
+   title="Hola mundo Hola mundo"
+   titleSubNav={[{id:1,title:"hola mm"},{id:2,title:"mundo"}]}/>
+    <NavLink 
+    index={[{id:2,state:false}]}
+   subCategory={true}
+   openSubCategory={openSub}
+   setOpenSubCategory={()=>setOpenSub(!openSub)}
+   title="Hola mundo Hola mundo"
+   titleSubNav={[{id:1,title:"hola deb"},{id:2,title:"jajhaj"}]}/> */
   )
 }
 
